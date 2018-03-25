3e24d6cf34a3060784597e77e9b009ac7ab5a407
Naffar, Nabil (nn605g) <nn605g@intl.att.com>
2018-03-25 18:02:20 +0300

    display images...

--- kitten-library/App.js
+++ kitten-library/App.js
@@ -1,18 +1,14 @@
+"use strict";
 import * as React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, ScrollView } from 'react-native';
+import Cat from './components/cat';
 export default class App extends React.Component {
     render() {
         return (React.createElement(View, { style: styles.container },
             React.createElement(Text, { style: styles.header }, "Kitten Library"),
             React.createElement(ImageBackground, { style: styles.container, source: require('./assets/main-screen-logo.jpg'), imageStyle: { opacity: 0.2 } },
-                React.createElement(View, { style: styles.view },
-                    React.createElement(Text, null, "test")),
-                React.createElement(View, null,
-                    React.createElement(Text, null, "test")),
-                React.createElement(View, null,
-                    React.createElement(Text, null, "test")),
-                React.createElement(View, null,
-                    React.createElement(Text, null, "test")))));
+                React.createElement(ScrollView, { contentContainerStyle: styles.contentContainer }, catsUrls.map((url, i) => (React.createElement(View, { style: styles.viewItem, key: i },
+                    React.createElement(Cat, { url: url }))))))));
     }
 }
 const styles = StyleSheet.create({
@@ -29,7 +25,32 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: 'bold'
     },
-    view: {
-        display: 'flex'
-    }
+    contentContainer: {
+        paddingVertical: 20,
+    },
+    viewItem: {
+        padding: 10,
+        width: 200,
+        height: 200
+    },
 });
+const catsUrls = [
+    require("./assets/images/a1.jpg"),
+    require("./assets/images/a2.jpg"),
+    require("./assets/images/a3.jpg"),
+    require("./assets/images/a4.jpg"),
+    require("./assets/images/a5.jpg"),
+    require("./assets/images/a6.jpg"),
+    require("./assets/images/a7.jpg"),
+    require("./assets/images/a8.jpg"),
+    require("./assets/images/a9.jpg"),
+    require("./assets/images/a10.jpg"),
+    require("./assets/images/a11.jpg"),
+    require("./assets/images/a12.jpg"),
+    require("./assets/images/a13.jpg"),
+    require("./assets/images/a14.jpg"),
+    require("./assets/images/a15.jpg"),
+    require("./assets/images/a16.jpg"),
+    require("./assets/images/a17.jpg"),
+    require("./assets/images/a18.jpg")
+];
